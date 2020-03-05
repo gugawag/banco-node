26c85593039ed096ebeae07a9e31096cf63ff981
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2020-03-05 11:08:29 -0300

    Inicio

---
+++ app/js/model/conta.js
@@ -0,0 +1,31 @@
+class Conta {
+
+    constructor(numero, saldo=0) {
+        this._numero = numero;
+        this._saldo = saldo;
+    }
+
+    get numero() {
+        return this._numero;
+    }
+
+    set numero(numero) {
+        this._numero = numero;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    debitar(valor) {
+        //apenas debita se houver saldo
+        if (valor < this._saldo) {
+            this._saldo -= valor;
+        }
+    }
+
+    creditar(valor) {
+        this._saldo += valor;
+    }
+
+}
